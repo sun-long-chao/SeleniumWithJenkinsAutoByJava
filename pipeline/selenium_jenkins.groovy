2071e2ad4951ca0731764dcd4e027e49d272c803
sun-long-chao <784966405@qq.com>
2019-12-13 17:48:14 +0800

    ...

--- pipeline/selenium_jenkins.groovy
+++ pipeline/selenium_jenkins.groovy
@@ -6,7 +6,7 @@ pipeline{
     parameters {
         string(name: 'BROWSER_TYPE', defaultValue: 'chrome', description: 'Type a browser type, should be chrome/firefox')
         string(name: 'TEST_SERVER_URL', defaultValue: '', description: 'Type the test server url')
-        string(name: 'NODE', defaultValue: 'win-anthony-demo', description: 'Please choose a windows node to execute this job.')
+        string(name: 'NODE', defaultValue: 'selenium-jenkins-demo', description: 'Please choose a windows node to execute this job.')
     }
     
 	stages{
@@ -22,25 +22,21 @@ pipeline{
 	    stage("Git Checkout"){
 	        //steps{
 	          //  script{
-	         //       node(win_node) {
-	         //            checkout([$class: 'GitSCM', branches: [[name: '*/master']],
+ 	         //             checkout([$class: 'GitSCM', branches: [[name: '*/master']],
 			//			    userRemoteConfigs: [[credentialsId: '6f4fa66c-eb02-46dc-a4b3-3a232be5ef6e', 
 			//				url: 'https://github.com/QAAutomationLearn/JavaAutomationFramework.git']]])
-	         //       }
 	         //   }
 	        //}
 	    }
 	    
         
-	    stage("Run Selenium Test"){
-	        steps{
-	            script{
-	                node(win_node){
-	                    run_bat = env.WORKSPACE + "\\run.bat"
-	                    bat (run_bat)
-	                }
-	            }
-	        }
-	    }
+	    //stage("Run Selenium Test"){
+	    //    steps{
+	     //       script{
+ 	     ////               run_bat = env.WORKSPACE + "\\run.bat"
+	     //               bat (run_bat)
+	     //       }
+	     //   }
+	   // }
 	}
 }
